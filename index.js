f5e5aff08ea78e72dd9bf9ee9a178d2d3f542c7b
unknown <=>
2023-04-05 17:15:03 +0700

    Mongoose connect to MongoDB

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const authRoutes = require('./routes/auth');
+
+app.use(authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.listen(8000, () => {
-  console.log('Server started');
-});
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000 // Timeout after 5s of not being able to connect
+  })
+  .then(result => {
+    console.log("test");
+    app.listen(8000);
+  })
+  .catch(err => {
+    console.log("Error: " + err);
+  });
---
+++ routes/auth.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const { check, body } = require('express-validator/check');
+
+const authController = require('../controllers/auth');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.post(
+    '/signup',
+    [
+      check('email')
+        .isEmail()
+        .withMessage('Please enter a valid email.')
+        .custom((value, { req }) => {
+          // if (value === 'test@test.com') {
+          //   throw new Error('This email address if forbidden.');
+          // }
+          // return true;
+          return User.findOne({ email: value }).then(userDoc => {
+            if (userDoc) {
+              return Promise.reject(
+                'E-Mail exists already, please pick a different one.'
+              );
+            }
+          });
+        })
+        .normalizeEmail(),
+      body(
+        'password',
+        'Please enter a password with only numbers and text and at least 5 characters.'
+      )
+        .isLength({ min: 5 })
+        .isAlphanumeric()
+        .trim(),
+      body('confirmPassword')
+        .trim()
+        .custom((value, { req }) => {
+          if (value !== req.body.password) {
+            throw new Error('Passwords have to match!');
+          }
+          return true;
+        })
+    ],
+    authController.postSignup
+  );
+  
+module.exports = router;
