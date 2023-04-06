2ee156c7d6c6d0955320093684ca60876ea9f4fd
Nicky Hendrik Sen <nicky.hendriksen@yahoo.com>
2023-04-06 23:21:49 +0700

    Login register

--- index.js
+++ index.js
@@ -2,19 +2,30 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
-app.use(bodyParser.urlencoded({ extended: false }));
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const authRoutes = require('./routes/auth');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cors());
+
 app.use(authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
+});
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
 });
 
 mongoose
@@ -24,7 +35,7 @@ mongoose
     serverSelectionTimeoutMS: 5000 // Timeout after 5s of not being able to connect
   })
   .then(result => {
-    console.log("test");
+    console.log("Connected");
     app.listen(8000);
   })
   .catch(err => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check, body } = require('express-validator/check');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
 const User = require('../models/user');
@@ -7,42 +7,49 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.post(
-    '/signup',
-    [
-      check('email')
-        .isEmail()
-        .withMessage('Please enter a valid email.')
-        .custom((value, { req }) => {
-          // if (value === 'test@test.com') {
-          //   throw new Error('This email address if forbidden.');
-          // }
-          // return true;
-          return User.findOne({ email: value }).then(userDoc => {
-            if (userDoc) {
-              return Promise.reject(
-                'E-Mail exists already, please pick a different one.'
-              );
-            }
-          });
-        })
-        .normalizeEmail(),
-      body(
-        'password',
-        'Please enter a password with only numbers and text and at least 5 characters.'
-      )
-        .isLength({ min: 5 })
-        .isAlphanumeric()
-        .trim(),
-      body('confirmPassword')
-        .trim()
-        .custom((value, { req }) => {
-          if (value !== req.body.password) {
-            throw new Error('Passwords have to match!');
+  '/register',
+  [
+    body('name')
+      .notEmpty()
+      .withMessage("Name is required."),
+    body('email')
+      .notEmpty()
+      .withMessage("Email is required.")
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        // if (value === 'test@test.com') {
+        //   throw new Error('This email address if forbidden.');
+        // }
+        // return true;
+        return User.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject(
+              'E-Mail exists already, please pick a different one.'
+            );
           }
-          return true;
-        })
-    ],
-    authController.postSignup
-  );
+        });
+      })
+      .normalizeEmail(),
+    body(
+      'password',
+      'Please enter a password with only numbers and text and at least 8 characters.'
+    )
+      .isLength({ min: 8 })
+      .isAlphanumeric()
+      .trim(),
+    body('confirmPassword')
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords have to match!');
+        }
+        return true;
+      })
+  ],
+  authController.register
+);
+
+router.post('/login', authController.login);
   
 module.exports = router;
